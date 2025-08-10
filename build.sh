fc0d90d0bdae06b3c1805f285fa705e17bf16601
adam <a-hanasalves@outlook.com>
2025-08-10 17:43:52 +0100

    built sdldoom and added load button

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd sdldoom-1.10
+export EMCC_CFLAGS="-std=c89 -sUSE_SDL"
+emconfigure ./configure
+emmake make
+emcc -o index.html ./*.o --preload-file doom1.wad -s ALLOW_MEMORY_GROWTH=1 --shell-file ../shell.html
+mv index.* ../
